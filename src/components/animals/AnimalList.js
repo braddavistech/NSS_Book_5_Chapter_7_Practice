87b42bfa001eddf371d7065a6c9160dd703928c0
braddavistech <brad@braddavistech.com>
2018-11-22 18:51:51 -0600

    completed b5c7 practice.

---
+++ src/components/animals/AnimalList.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import { Link } from "react-router-dom";
+import APITools from "../../modules/APITools";
+import "./AnimalList.css";
+import dog from "./DogIcon.png"
+
+
+class AnimalList extends Component {
+  render() {
+
+    return (
+      <div id="ApplicationView">
+        <header>CURRENT ANIMALS</header>
+        {
+          this.props.animals.map(animal =>
+            <div key={animal.id} className="animalCard">
+              <div className="card-body">
+                <h5 >
+                  <img src={dog} className="icon--dog" alt="Dog Icon" />
+                  <Link className="nav-link" to={`/animals/${animal.id}`}><button className="detailsAnimal">Details</button></Link>
+                  <button id="deleteAnimal" href="#"
+                    onClick={() => APITools.deleteItem("animals", animal.id)
+                      .then(() => this.props.refresh())}
+                    className="card-link">Checked Out</button>
+                </h5>
+                <h4>{animal.name}</h4>
+                <h5>{animal.breed}</h5>
+              </div>
+            </div>
+          )
+        }
+      </div>
+    )
+  }
+
+
+}
+
+
+export default AnimalList
